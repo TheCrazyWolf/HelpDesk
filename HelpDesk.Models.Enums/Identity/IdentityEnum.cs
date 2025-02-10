3e1fd41a7f88fe4bee491d96c07ae6177ad9622d
kulagin <alex_kulagin@samgk.ru>
2025-02-10 17:18:05 +0400

    Атрибуты

--- HelpDesk.Models.Enums/Identity/IdentityEnum.cs
+++ HelpDesk.Models.Enums/Identity/IdentityEnum.cs
@@ -1,8 +1,13 @@
-﻿namespace HelpDesk.Models.Enums.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelpDesk.Models.Enums.Identity;
 
 public enum IdentityEnum
 {
+    [Display(Name = "Студент")]
     Student,
+    [Display(Name = "Преподаватель")]
     Teacher,
+    [Display(Name = "ИТ отдел")]
     TechSupport
 }
