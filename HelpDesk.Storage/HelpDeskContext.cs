8d449b56caf22e8ddce7ddf2a3fde060cdc770b2
kulagin <alex_kulagin@samgk.ru>
2025-02-10 17:26:29 +0400

    Проект с таблицами

---
+++ HelpDesk.Storage/HelpDeskContext.cs
@@ -0,0 +1,19 @@
+﻿using HelpDesk.Models.DLA.Identity;
+using HelpDesk.Models.DLA.Tickets;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelpDesk.Storage;
+
+public class HelpDeskContext : DbContext
+{
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Ticket> Tickets { get; set; }
+    public DbSet<TicketExecutor> TicketExecutors { get; set; }
+    public DbSet<TicketHistory> TicketHistories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=helpdesk.db");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
