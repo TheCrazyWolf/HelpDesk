4f03fec812328c1f7854f5059a41613e36fc1f3f
TheCrazyWolf <thecrazywolf63@gmail.com>
2025-02-11 21:19:36 +0400

    Обновление сервиса + обновление данных профиля

--- HelpDesk.Services/Identity/IdentityService.cs
+++ HelpDesk.Services/Identity/IdentityService.cs
@@ -1,5 +1,4 @@
-﻿using HelpDesk.Models.Dto.Auth;
-using HelpDesk.Models.Dto.Identity;
+﻿using HelpDesk.Models.Dto.Identity;
 using HelpDesk.Models.Enums.Identity;
 using HelpDesk.Models.PLA.Account;
 using HelpDesk.Services.Utils;
@@ -23,21 +22,29 @@ public class IdentityService(HelpDeskContext ef, IMapper mapper)
         return identity is not null ? mapper.Map<IdentityView>(identity) : null;
     }
 
+    public async Task UpdateContactData(IdentityUpdateContact contactData)
+    {
+        var user = await ef.Accounts.FirstOrDefaultAsync(x => x.Id == contactData.Id);
+        if (user is null) throw new NullReferenceException("Пользователь не найден");
+        mapper.Map(contactData, user);
+        await ef.SaveChangesAsync();
+    }
+    
     public async Task ChangeRole(IdentityChaneRole changeRole)
     {
         var user = await ef.Accounts.FirstOrDefaultAsync(x => x.Id == changeRole.Id);
-        if (user is null) throw new NullReferenceException("User does not exist");
+        if (user is null) throw new NullReferenceException("Пользователь не найден");
         mapper.Map(changeRole, user);
         await ef.SaveChangesAsync();
     }
-
-
+    
     public async Task ChangeLogin(IdentityChangeLogin changeLogin)
     {
         var user = await ef.Accounts.FirstOrDefaultAsync(x => x.Id == changeLogin.Id);
-        if (user is null) throw new NullReferenceException("User does not exist");
+        if (user is null) throw new NullReferenceException("Пользователь не найден");
+        if(user.IsMfcIntegation) throw new NullReferenceException("Эта учётная связана с mfc.samgk.ru. Менять логины или пароль запрещено.");
         var newLogin = await ef.Accounts.FirstOrDefaultAsync(x => x.Login == changeLogin.UserName);
-        if (newLogin is not null) throw new NullReferenceException("This is login is already in use");
+        if (newLogin is not null) throw new NullReferenceException("Этот логин уже используется");
         mapper.Map(changeLogin, user);
         await ef.SaveChangesAsync();
     }
@@ -45,7 +52,8 @@ public class IdentityService(HelpDeskContext ef, IMapper mapper)
     public async Task ChangePassword(IdentityChangePassword changePassword)
     {
         var user = await ef.Accounts.FirstOrDefaultAsync(x => x.Id == changePassword.Id);
-        if (user is null) throw new NullReferenceException("User does not exist");
+        if (user is null) throw new NullReferenceException("Пользователь не найден");
+        if(user.IsMfcIntegation) throw new NullReferenceException("Эта учётная связана с mfc.samgk.ru. Менять логины или пароль запрещено.");
         changePassword.Password = changePassword.Password.GetHash();
         mapper.Map(changePassword, user);
         await ef.SaveChangesAsync();
