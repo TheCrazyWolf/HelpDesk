0876ec504397c385b5bf87a6cc53290042c39c00
kulagin <alex_kulagin@samgk.ru>
2025-02-11 17:27:18 +0400

    Страница авторизации

--- HelpDesk.Models.Dto/Auth/LoginParams.cs
+++ HelpDesk.Models.Dto/Auth/LoginParams.cs
@@ -1,8 +1,10 @@
-﻿namespace HelpDesk.Models.Dto.Auth;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelpDesk.Models.Dto.Auth;
 
 public class LoginParams
 {
-    public string Username { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    [StringLength(35, MinimumLength = 3)] public string Username { get; set; } = string.Empty;
+    [StringLength(35, MinimumLength = 3)] public string Password { get; set; } = string.Empty;
     public bool IsRememberMe { get; set; }
 }
