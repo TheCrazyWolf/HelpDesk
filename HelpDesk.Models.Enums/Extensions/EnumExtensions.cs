f5054f68988688dd5e286f8efafef3c2c3e123e3
kulagin <alex_kulagin@samgk.ru>
2025-02-12 08:17:30 +0400

    Изменения в layout

---
+++ HelpDesk.Models.Enums/Extensions/EnumExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace HelpDesk.Models.Enums.Extensions;
+
+public static class EnumExtensions
+{
+    public static string GetDisplayName(this Enum value)
+    {
+        return value.GetType()
+            .GetMember(value.ToString())
+            .FirstOrDefault()?
+            .GetCustomAttribute<DisplayAttribute>()?
+            .GetName() ?? value.ToString();
+    }
+
+    public static string GetDescription(this Enum value)
+    {
+        return value.GetType()
+            .GetMember(value.ToString())
+            .FirstOrDefault()?
+            .GetCustomAttribute<DisplayAttribute>()?
+            .GetDescription() ?? value.ToString();
+    }
+}
