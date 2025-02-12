f28196cd6f32b9841b23e6d4f0190fec756e8e1a
kulagin <alex_kulagin@samgk.ru>
2025-02-12 08:21:10 +0400

    Fix сущеностей

--- HelpDesk.Models.DLA/Devices/DeviceInUseTicket.cs
+++ HelpDesk.Models.DLA/Devices/DeviceInUseTicket.cs
@@ -1,8 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using HelpDesk.Models.DLA.Common;
+using HelpDesk.Models.DLA.Tickets;
 
 namespace HelpDesk.Models.DLA.Devices;
 
-public class DeviceInUseTicket
+public class DeviceInUseTicket : DlaEntity
 {
     public string? DeviceId { get; set; }
     [ForeignKey("DeviceId")] public Device? Device { get; set; }
